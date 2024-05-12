c6494ac64a56c8be4017642094ab6de7a6a35157
inv1x <pavlik.vlad.i@gmail.com>
2024-05-12 20:22:03 +0300

    Initial Commit

---
+++ SwiftUI-Apple-Frameworks/SwiftUI_Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Apple_FrameworksApp.swift
+//  SwiftUI-Apple-Frameworks
+//
+//  Created by Inv1x on 12.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
